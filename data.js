d04f33dd92fa15cc7ebd3aa3e6eb6e76a78344b9
Pablo Cantillana <pablocantr@gmail.com>
2026-06-14 15:29:53 -0400

    Paso 1: Agregar archivo de datos iniciales (data.js)

---
+++ data.js
@@ -0,0 +1,17 @@
+
+let tareas = [
+    {
+        id: 1,
+        titulo: "Aprender Node.js",
+        descripcion: "Entender los fundamentos del backend",
+        completada: false
+    },
+    {
+        id: 2,
+        titulo: "Hacer la tarea del IPG",
+        descripcion: "Crear API RESTful con Express",
+        completada: true
+    }
+];
+
+module.exports = tareas;
